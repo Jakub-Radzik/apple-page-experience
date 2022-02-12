df4c88b1cf577d6744af04508aa31a684d68d0f3
Jakub Radzik <jradzik4@gmail.com>
2022-02-12 14:09:31 +0100

    Next generation

--- src/App.tsx
+++ src/App.tsx
@@ -8,5 +8,7 @@ function App() {
     </div>
   );
 }
+// feature with more:
+// https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/05-flip-for-nc/0063.jpg
 
 export default App;
